ea7bdbba78d7c1d2905981cc36e3a72ce4f30a62
Caio Serra <caio_mello@id.uff.br>
2021-07-15 22:09:52 -0300

    add links to navbar

--- components/header.js
+++ components/header.js
@@ -4,28 +4,46 @@ import styles from "./header.module.scss";
 export default function HeaderComponent(props) {
   return (
     <header className={styles.header}>
-      <nav className={styles.first_nav} >
+      <nav className={styles.first_nav}>
         <ul>
           <li>
-            <NavLink onClick={() => props.set_current_page('about')}>Sobre</NavLink>
+            <NavLink onClick={() => props.set_current_page("about")}>
+              Sobre
+            </NavLink>
           </li>
           <li>
-            <NavLink onClick={() => props.set_current_page('rewards')} >Recompensas</NavLink>
+            <NavLink onClick={() => props.set_current_page("rewards")}>
+              Recompensas
+            </NavLink>
           </li>
           <li>
-            <NavLink onClick={() => props.set_current_page('events')}>Eventos</NavLink>
+            <NavLink onClick={() => props.set_current_page("events")}>
+              Eventos
+            </NavLink>
           </li>
           <li>
-            <NavLink onClick={() => props.set_current_page('events')} >Contato</NavLink>
+            <NavLink onClick={() => props.set_current_page("contact")}>
+              Contato
+            </NavLink>
           </li>
         </ul>
       </nav>
-      <div onClick={() => props.set_current_page('home')} className={styles.logo}>
-        <img  src="orlla-logo-original.svg"></img>
+      <div
+        onClick={() => props.set_current_page("home")}
+        className={styles.logo}
+      >
+        <img src="orlla-logo-original.svg"></img>
       </div>
       <nav className={styles.second_nav}>
-        <button className={"btn btn-secondary "+styles.sign_in_button}>Login</button>
-        <button className={"btn btn-primary "+styles.sign_up_button}>Cadastre-se</button>
+        <button className={"btn btn-secondary " + styles.sign_in_button}>
+          Login
+        </button>
+        <button
+          onClick={() => props.set_current_page("register")}
+          className={"btn btn-primary " + styles.sign_up_button}
+        >
+          Cadastre-se
+        </button>
       </nav>
     </header>
   );
