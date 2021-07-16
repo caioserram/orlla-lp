3cd46ab3b6c80802ff5e6f9f29e9822daf1da67f
Caio Serra <caio_mello@id.uff.br>
2021-07-15 21:16:07 -0300

    fix some about-card styling bugs

--- components/about_card.js
+++ components/about_card.js
@@ -2,7 +2,9 @@ import styles from "./about_card.module.scss";
 
 export default function AboutCardComponent(props) {
   return (
-    <div className={`col-sm-4 ${styles.card_container}`}>
+    <div
+      className={`col-3 ${styles.card_container}`}
+    >
       <div className={`card ${styles.custom_card}`}>
         <div className={`card-body ${styles.card_body_div}`}>
           <div className={styles.card_title}>
@@ -12,9 +14,9 @@ export default function AboutCardComponent(props) {
           <div className={styles.card_body_text}>
             <p>{props.body}</p>
           </div>
-          <div className={styles.card_footer}>
-            <a href="#">Explore →</a>
-          </div>
+        </div>
+        <div className={styles.card_footer}>
+          <a href="#">Explore →</a>
         </div>
       </div>
     </div>
