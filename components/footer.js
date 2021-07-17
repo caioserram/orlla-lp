4c8b2ad70652462a6d0a715e42fff93d56871527
Caio Serra <caio_mello@id.uff.br>
2021-07-17 15:39:23 -0300

    removes footer component

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.scss";
-
-export default function FooterComponet() {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.footer_text}>{new Date().getFullYear()} - Orlla </div>
-    </footer>
-  );
-}
