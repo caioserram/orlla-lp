25eadf6a1d389d09f313fec19fa3e2fad7172f4b
Caio Serra <caio_mello@id.uff.br>
2021-07-19 19:23:38 -0300

    implement responsiveness at about page

--- components/about.js
+++ components/about.js
@@ -1,6 +1,6 @@
 import styles from "./about.module.scss";
 import AboutCardComponent from "./about_card";
-import { Carousel } from "react-bootstrap";
+import { CardGroup, Carousel, Row } from "react-bootstrap";
 
 export default function AboutComponent() {
   const prevIcon = (
@@ -15,35 +15,43 @@ export default function AboutComponent() {
     </span>
   );
 
+  const aboutComponents = [
+    <AboutCardComponent
+      title="Ganhe Benefícios"
+      body="Você pode trocar sua ajuda por benefícios como WiFi, cadeira de praia, vale compras, entre outros."
+      img_ref="catando_lixo.png"
+    ></AboutCardComponent>,
+    <AboutCardComponent
+      title="Ajude Uma Causa"
+      body="Receba notificações e participe de iniciativas para limpeza das praias e oceanos mais próximas da sua região."
+      img_ref="lixo_praia.png"
+    ></AboutCardComponent>,
+    <AboutCardComponent
+      title="Seja Parte da Mudança"
+      body="Faça parte de uma comunidade de pessoas que busca melhorar o mundo através de pequenas ações. "
+      img_ref="catando_lixo_2.png"
+    ></AboutCardComponent>,
+  ];
+
+  const carouselItems = aboutComponents.map((item) => (
+    <Carousel.Item> {item} </Carousel.Item>
+  ));
+
+  const cardItems = aboutComponents.map((item) => <div> {item} </div>);
+
   return (
     <div>
-      <h1 className={styles.title}> A mudança no mundo começa por você </h1>
-      <Carousel prevLabel={null} nextLabel={null} prevIcon={prevIcon} nextIcon={nextIcon}>
-        <Carousel.Item>
-          <AboutCardComponent
-            title="Ganhe Benefícios"
-            body="Você pode trocar sua ajuda por benefícios como WiFi, cadeira de praia, vale compras, entre outros."
-            img_ref="catando_lixo.png"
-            layot_class="d-block w-100"
-          ></AboutCardComponent>
-        </Carousel.Item>
-        <Carousel.Item>
-          <AboutCardComponent
-            title="Ajude Uma Causa"
-            body="Receba notificações e participe de iniciativas para limpeza das praias e oceanos mais próximas da sua região."
-            img_ref="lixo_praia.png"
-            layot_class="d-block w-100"
-          ></AboutCardComponent>
-        </Carousel.Item>
-        <Carousel.Item>
-          <AboutCardComponent
-            title="Seja Parte da Mudança"
-            body="Faça parte de uma comunidade de pessoas que busca melhorar o mundo através de pequenas ações. "
-            img_ref="catando_lixo_2.png"
-            layot_class="d-block w-100"
-          ></AboutCardComponent>
-        </Carousel.Item>
+      <h1 className={`mb-4 ${styles.title}`}> A mudança no mundo começa por você </h1>
+      <Carousel
+        prevLabel={null}
+        nextLabel={null}
+        prevIcon={prevIcon}
+        nextIcon={nextIcon}
+        className="d-md-none"
+      >
+        {carouselItems}
       </Carousel>
+      <Row xs={1} md={3} className="d-none d-md-flex bg-white">{cardItems}</Row>
     </div>
   );
 }
