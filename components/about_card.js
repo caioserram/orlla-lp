6436e999e861b94df07c5e68daceb0cc2b36d776
Caio Serra <caio_mello@id.uff.br>
2021-07-19 19:22:50 -0300

    refactor about card to use react-bootstrap components

--- components/about_card.js
+++ components/about_card.js
@@ -1,8 +1,30 @@
+import { Card, Nav } from "react-bootstrap";
 import styles from "./about_card.module.scss";
 
 export default function AboutCardComponent(props) {
   return (
-    <div
+    <Card className="bg-white">
+      <Card.Body className="bg-white d-flex flex-wrap text-left">
+        <Card.Title className="bg-white text-primary font-weight-bold mt-3 ">
+          {props.title}
+        </Card.Title>
+        <div className="bg-white w-100  mt-3 mb-3 d-flex align-items-center justify-content-center">
+          <Card.Img
+            className={`bg-white ${styles.img_width}`}
+            src={props.img_ref}
+          />
+        </div>
+        <Card.Text className="bg-white ">{props.body}</Card.Text>
+      </Card.Body>
+      <Nav className="bg-white mb-3 mt-3">
+        <Nav.Link className="bg-white text-primary">Explore →</Nav.Link>
+      </Nav>
+    </Card>
+  );
+}
+
+{
+  /* <div
       className={`${props.layout_class} ${styles.card_container}`}
     >
       <div className={`card ${styles.custom_card}`}>
@@ -19,6 +41,5 @@ export default function AboutCardComponent(props) {
           <a href="#">Explore →</a>
         </div>
       </div>
-    </div>
-  );
+    </div> */
 }
