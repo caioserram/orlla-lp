06a61601b83ba90c4d7571b9087b22226e5108ba
Caio Serra <caio_mello@id.uff.br>
2021-07-20 11:29:16 -0300

    removes unused state from index.js

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,7 @@ import MainLayoutComponent from "../components/main_layout";
 import About from "./about";
 
 
-function chooseCurrentPage(current_page){
-  switch (current_page) {
-    case "home":
-      return (<HomeComponent />)
-    case "about":
-      return (<AboutComponent/>);
-    case "register":
-      return (<RegisterComponent/>);
-    default:
-      return (<HomeComponent />)
-  }
-
-}
-
 export default function Home() {
-  const [current_page, set_current_page] = useState("home");
 
   return (
     <MainLayoutComponent>
