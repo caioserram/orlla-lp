825d388eb44a58053f268b5cf2fec54ee92d8ccc
Caio Serra <caio_mello@id.uff.br>
2021-07-21 20:42:59 -0300

    changes top event cards title class

--- components/event_top_cards.js
+++ components/event_top_cards.js
@@ -2,21 +2,23 @@ import { Card } from "react-bootstrap";
 
 export default function EventTopCardComponent(props) {
   return (
-    <Card className="bg-white p-4 border-0">
+    <Card className="bg-white p-2 border-0">
       <div className="bg-white w-100 mt-3 mb-3 d-flex align-items-center justify-content-center">
         <Card.Img
-          className="bg-white text-center w-50 h-50"
+          className="bg-white text-center w-25 h-25"
           variant="top"
           src={props.img_ref}
         />
       </div>
       <Card.Body className="bg-white d-block">
         <Card.Title className="bg-white d-flex align-items-center justify-content-center text-center">
-          <h1 className="bg-white text-primary display-3">
-            <b className="bg-white">{props.title}</b>
+          <h1 className="bg-white text-primary display-5 fw-bold">
+            {props.title}
           </h1>
         </Card.Title>
-        <Card.Text className="bg-white mt-3 d-flex align-items-center justify-content-center text-center">{props.text}</Card.Text>
+        <Card.Text className="bg-white mt-3 d-flex align-items-center justify-content-center text-center">
+          {props.text}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
